2f607577d9aa157dc3460c6d8772af29ca05f9c0
ebsagdic42 <84442369+ebsagdic42@users.noreply.github.com>
2024-10-13 20:14:01 +0300

    Data Katmanı Hazırlandı

--- AuthServer.Core/Models/UserApp.cs
+++ AuthServer.Core/Models/UserApp.cs
@@ -9,6 +9,6 @@ namespace AuthServer.Core.Models
 {
     public  class UserApp:IdentityUser
     {
-        public string UserId { get; set; }
+        public string City { get; set; }
     }
 }
---
+++ AuthServer.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using AuthServer.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepositoy<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        //Service katmanında bu Repository classını DI olarak geçince UnitOfWork ile nerde commitasync veya commit çağırırsam orda dbye yansıyacak
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            var entity =  await _dbSet.FindAsync(id);
+            //EntityState.Detached yapısını service class'sını anlatırken detaylandıracağım.
+            if(entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            return entity;
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            _context.Entry(entity).State |= EntityState.Modified; 
+            return entity;
+        }
+
+        public IQueryable<TEntity> Where(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}
